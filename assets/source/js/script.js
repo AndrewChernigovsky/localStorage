dca67f9cfd9959b8ba43b3bbd1bfaa3139c2db26
UserRF <you@example.com>
2022-02-15 15:21:27 +0300

    1

--- assets/source/js/script.js
+++ assets/source/js/script.js
@@ -3,15 +3,24 @@ const wrraperData = document.querySelector('.wrapperData');
 const inputsData = wrraperData.getElementsByTagName('input');
 const btn = document.getElementById('dataFormsubmit');
 
-const elValue = [];
-
-
 
 const localInputs = function localInputs() {
-    Array.from(elValue).forEach(e => {
-        e.value = elValue;
+    let eName = []
+
+    Array.from(inputsData).forEach(e => {
+       
+
+        const elValue = {
+            key: e.name,
+            value: e.value
+        }
+
+        eName.push(elValue);
     })
-    localStorage.setItem('key', JSON.stringify(elValue));
+
+    localStorage.setItem('key', JSON.stringify(eName));
 }
 
 form.addEventListener('submit', localInputs)
+
+console.log(localInputs())
